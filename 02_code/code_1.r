d1f7a0ed62c100da726809fb465fb0e75f31c69b
HirokiIto05 <ito.hiroki.lf@gmail.com>
2025-07-30 22:17:49 +0900

    initial renv

--- 02_code/code_1.r
+++ 02_code/code_1.r
@@ -1,1 +1,14 @@
-library(dplyr)
+df <- data.frame(
+  "a_income" = c(1000, 2000, 3000),
+  "a_rent" = c(500, 600, 700),
+  "b_income" = c(1500, 2500, 3500),
+  "b_rent" = c(800, 900, 1000)
+)
+
+list_vars <- dplyr::select_vars(vars = names(df), starts_with("a"))
+
+dplyr::select(df, list_vars)
+
+renv::snapshot()
+
+
